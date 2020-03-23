68520e4426ab79668a49116d3118b572aeb3af7e
MatthewOwens <matthew@owens.tech>
2020-03-23 13:58:45 +0000

    wip

--- src/clogs.c
+++ src/clogs.c
@@ -7,23 +7,23 @@
 struct clogs
 {
 	// queues
-	char* 				msg_q[CLOGS_QUEUE_MAX];
-	enum clogs_level	lvl_q[CLOGS_QUEUE_MAX];
-	char* 				fnc_q[CLOGS_QUEUE_MAX];
+	char* 				msg_q[CLOGS_QUEUE_MAX+1];
+	enum clogs_level	lvl_q[CLOGS_QUEUE_MAX+1];
+	char* 				fnc_q[CLOGS_QUEUE_MAX+1];
 	// trackers
 	int first, last, count;
 	// concurrency
-	mtx_t put_mtx;
-	mtx_t pop_mtx;
+	//mtx_t put_mtx;
+	//mtx_t pop_mtx;
 
 	FILE* logfile;
 };
 
 static char* levels[] = 
 {
-	"[info] ",
-	"[warn] ",
-	"[error] "
+	"[info]",
+	"[warn]",
+	"[error]"
 };
 
 static FILE* streams[3];
@@ -38,10 +38,15 @@ static struct clogs clog;
 
 void clog_put(enum clogs_level lvl, const char* fnc, const char* msg)
 {
-	mtx_lock(&clog.put_mtx);
+	//mtx_lock(&clog.put_mtx);
 
 	if(clog.count < CLOGS_QUEUE_MAX - 1) {
 		clog.last = (clog.last +1) % CLOGS_QUEUE_MAX;
+
+		printf(__FUNCTION__);
+		printf("\n\t - msg is '%s'\n", msg);
+		printf("\t - clog.last is %d\n", clog.last);
+
 		clog.msg_q[clog.last] = msg;
 		clog.lvl_q[clog.last] = lvl;
 		clog.fnc_q[clog.last] = fnc;
@@ -51,36 +56,37 @@ void clog_put(enum clogs_level lvl, const char* fnc, const char* msg)
 		fprintf(stderr, ": clogs q full!\n");
 	}
 
-	mtx_unlock(&clog.put_mtx);
+	//mtx_unlock(&clog.put_mtx);
 }
 
 void stream_print()
 {
 	int i = clog.first;
-	fprintf(streams[clog.lvl_q[i]],colors[clog.lvl_q[i]]);
-	fprintf(streams[clog.lvl_q[i]],levels[clog.lvl_q[i]]);
-	fprintf(streams[clog.lvl_q[i]],"(");
-	fprintf(streams[clog.lvl_q[i]],clog.fnc_q[i]);
-	fprintf(streams[clog.lvl_q[i]],")");
-	fprintf(streams[clog.lvl_q[i]], clog.msg_q[i]);
-	fprintf(streams[clog.lvl_q[i]],ANSI_COLOR_RESET);
-	fprintf(streams[clog.lvl_q[i]],"\n");
+	char* msg[100];
+
+	snprintf(msg, 100, "%s%s (%s) '%s'%s\n",
+			colors[clog.lvl_q[i]],
+			levels[clog.lvl_q[i]],
+			clog.fnc_q[i],
+			clog.msg_q[i],
+			ANSI_COLOR_RESET);
+	fprintf(streams[clog.lvl_q[i]], msg);
 }
 
 void file_print()
 {
 	int i = clog.first;
-	fprintf(clog.logfile,levels[clog.lvl_q[i]]);
-	fprintf(clog.logfile,"(");
-	fprintf(clog.logfile,clog.fnc_q[i]);
-	fprintf(clog.logfile,")");
-	fprintf(clog.logfile, clog.msg_q[i]);
-	fprintf(clog.logfile,"\n");
+	char* msg[100];
+
+	snprintf(msg, 100, "%s (%s) '%s'\n",
+			levels[clog.lvl_q[i]],
+			clog.fnc_q[i],
+			clog.msg_q[i]);
 }
 
 void clogs_pop()
 {
-	mtx_lock(&clog.pop_mtx);
+	//mtx_lock(&clog.pop_mtx);
 
 	// printing clog
 	if(clog.count > 0){
@@ -94,7 +100,7 @@ void clogs_pop()
 	clog.first = (clog.first + 1) % CLOGS_QUEUE_MAX;
 	clog.count--;
 
-	mtx_unlock(&clog.pop_mtx);
+	//mtx_unlock(&clog.pop_mtx);
 }
 
 void clogs_init(const char* logfile)
@@ -104,15 +110,15 @@ void clogs_init(const char* logfile)
 	}
 
 	clog.first = 0;
-	clog.last = CLOGS_QUEUE_MAX;
+	clog.last = CLOGS_QUEUE_MAX - 1;
 	clog.count = 0;
 
 	streams[0] = stdout;
 	streams[1] = stdout;
 	streams[2] = stderr;
 
-	mtx_init(&clog.put_mtx, mtx_plain);
-	mtx_init(&clog.pop_mtx, mtx_plain);
+	//mtx_init(&clog.put_mtx, mtx_plain);
+	//mtx_init(&clog.pop_mtx, mtx_plain);
 }
 
 void clogs_update()
@@ -131,8 +137,8 @@ void clogs_close()
 		fclose(clog.logfile);
 	}
 
-	mtx_destroy(&clog.put_mtx);
-	mtx_destroy(&clog.pop_mtx);
+	//mtx_destroy(&clog.put_mtx);
+	//mtx_destroy(&clog.pop_mtx);
 }
 
 void clogs_put(enum clogs_level l, const char* func, const char* msg)
--- tests/main.c
+++ tests/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "clogs.h"
 #include "threads.h"
+#include <string.h>
+#include <stdlib.h>
 #define THREAD_MAX 15
 
 thrd_t threads[THREAD_MAX];
 
 void foo(void* data)
 {
-	char msg[32] = { '\0' };
-	int *i = (int *)data;
-	strcpy(msg, "clogs test ");
-	//strcpy(msg+11, (*i) - '0');
+	//char* msg = "this is a test";
+	char msg[5];
+	int* thrd = (int *)data;
+	//printf("thrd is [%d]\n", *thrd);
+	snprintf(msg, 5, "%d", *thrd);
 	CLOG_ERR(msg);
 }
 
@@ -26,7 +29,7 @@ int main()
 	//}
 	int i = 5;
 	foo(&i);
-	foo(&i);
+	//foo(&i);
 
 	clogs_close();
 	return 0;
