4afe2d558ae9c51ff74deb2479494805c5ce8e3a
MatthewOwens <matthew@owens.tech>
2020-03-24 22:15:01 +0000

    merged [tests] branch

--- tests/main.c
+++ tests/main.c
@@ -1,33 +1,9 @@
-#include <stdio.h>
-#include "clogs.h"
-#include <pthread.h>
-#include <string.h>
+#include "runner.h"
+#include "check_clogs.h"
 #include <stdlib.h>
-
-#define THREAD_MAX 15
-pthread_t threads[THREAD_MAX];
-int tid[THREAD_MAX];
-
-void foo(void* data)
-{
-	int* thrdn = (int *)data;
-	CLOG("thread %d", *thrdn);
-	CLOG_WARN("thread %d", *thrdn);
-	CLOG_ERR("thread %d", *thrdn);
-}
 
 int main()
 {
-	clogs_init("/tmp/clogs.out");
-	for(int i = 0; i < THREAD_MAX; ++i){
-		tid[i] = i;
-		pthread_create(&threads[i], NULL, foo, &tid[i]);
-	}
-
-	for(int i = 0; i < THREAD_MAX; ++i){
-		pthread_join(threads[i], NULL);
-	}
-
-	clogs_close();
-	return 0;
+	int failed_count = run_suite_forkless(clogs_suite());
+	return (failed_count == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
